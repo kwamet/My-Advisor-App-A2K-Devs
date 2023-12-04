dc1726b8a7e18abfc5143b41bab0aeff8a8e3882
Kushraj Pollard <kushrajpollard@gmail.com>
2023-12-03 21:52:51 -0400

    Added login manager to init to handle user logins

--- App/controllers/__init__.py
+++ App/controllers/__init__.py
@@ -10,3 +10,11 @@ from .studentCourseHistory import *
 from .coursePlanCourses import *
 from .coursesOfferedPerSem import *
 from .coursePlan import *
+
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+
+def setup_flask_login(app):
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
