26e74ee5698dc9473341b03f8c76c0f4a1c93495
Kushraj Pollard <kushrajpollard@gmail.com>
2023-12-03 21:50:13 -0400

    Updated main and init for login and signup

--- App/__init__.py
+++ App/__init__.py
@@ -2,3 +2,11 @@ from .models import *
 from .views import *
 from .controllers import *
 from .main import *
+
+from App.models.user import User
+from App.controllers import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    # Load the user from the database using the user ID
+    return User.query.get(int(user_id))
--- App/main.py
+++ App/main.py
@@ -1,13 +1,14 @@
 import os
-from flask import Flask
-from flask_login import LoginManager, current_user
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, current_user, login_user
 from flask_uploads import DOCUMENTS, IMAGES, TEXT, UploadSet, configure_uploads
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 from datetime import timedelta
 
-from App.database import init_db
+#from App.database import init_db
+from App.database import db
 from App.config import config
 
 from App.controllers import (
@@ -16,6 +17,7 @@ from App.controllers import (
 )
 
 from App.views import views
+from App.models.user import User
 
 def add_views(app):
     for view in views:
@@ -40,8 +42,64 @@ def create_app(config_overrides={}):
     photos = UploadSet('photos', TEXT + DOCUMENTS + IMAGES)
     configure_uploads(app, photos)
     add_views(app)
-    init_db(app)
+    db.init_app(app)
+    #init_db(app)
     setup_jwt(app)
     setup_flask_login(app)
+
+    @app.route('/signup')
+    def signup():
+        return render_template('signup.html')
+
+    @app.route('/signup', methods=['POST'])
+    def handle_signup():
+        if request.method == 'POST':
+            # Retrieve form data
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            new_user = User(username=username, password=password)
+            user = User.query.filter_by(username=username).first()
+            db.session.add(new_user)
+            db.session.commit()
+
+            # Redirect to a user dashboard
+            return redirect(url_for('staff_dashboard'))
+
+        # Handle GET requests or form submission failure
+        return render_template('signup.html')
+    
+    def load_user(user_id):
+    # Load the user from the database using the user ID
+        return User.query.get(int(user_id))
+
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            # Retrieve form data
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            # Perform login logic (e.g., validate credentials)
+            user = User.query.filter_by(username=username).first()
+
+            if user and user.check_password(password):
+                # Log in the user
+                login_user(user)
+                # Redirect to a success page or another route
+                return redirect(url_for('staff_dashboard'))
+
+        # Handle GET requests or form submission failure
+        return render_template('login.html')
+
+    @app.route('/login/success')
+    def login_success():
+        return render_template('login_success.html')
+
+    @app.route('/staff-dashboard')
+    def staff_dashboard():
+        return render_template('staffDashboard.html')
+
     app.app_context().push()
     return app
