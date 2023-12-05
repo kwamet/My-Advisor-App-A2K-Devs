087ed95476319096efe611258b23534674bcf5f5
Kushraj Pollard <kushrajpollard@gmail.com>
2023-12-04 21:47:07 -0400

    main.py updated

--- App/main.py
+++ App/main.py
@@ -16,16 +16,25 @@ from App.controllers import (
     setup_flask_login
 )
 
-from App.views import views
 from App.models.user import User
 from App.models.courses import Course
 from App.models.program import Program
 from App.models.staff import Staff
 from App.models.student import Student
 
+from App.views import views
+
+from App.controllers.user import create_user, get_user, get_user_by_username
+from App.controllers.staff import create_staff, get_staff_by_id, verify_staff
+from App.controllers.student import create_student, get_student, verify_student
+from App.controllers.courses import create_course, get_course_by_courseCode
+from App.controllers.program import create_program, get_program_by_name, get_all_courses
+
+
 def add_views(app):
     for view in views:
         app.register_blueprint(view)
+
 
 def configure_app(app, config, overrides):
     for key, value in config.items():
@@ -33,6 +42,7 @@ def configure_app(app, config, overrides):
             app.config[key] = overrides[key]
         else:
             app.config[key] = config[key]
+
 
 def create_app(config_overrides={}):
     app = Flask(__name__, static_url_path='/static')
@@ -51,9 +61,11 @@ def create_app(config_overrides={}):
     setup_jwt(app)
     setup_flask_login(app)
 
+
     @app.route('/signup')
     def signup():
         return render_template('signup.html')
+
 
     @app.route('/signup', methods=['POST'])
     def handle_signup():
@@ -63,12 +75,10 @@ def create_app(config_overrides={}):
             password = request.form.get('password')
             user_type = request.form['user_type']
 
-            new_user = User(username=username, password=password)
-            user = User.query.filter_by(username=username).first()
-            db.session.add(new_user)
-            db.session.commit()
+            create_user(username, password)
 
             if user_type == 'student':
+                #create_student("id", password, username, "-")
                 new_student = Student(username=username, password=password, name=username, program_id="-")
                 db.session.add(new_student)
 
@@ -76,20 +86,16 @@ def create_app(config_overrides={}):
                 return redirect(url_for('student_dashboard'))
 
             elif user_type == 'staff':
+                #create_staff(password, "id", username)
                 new_staff = Staff(password=password, staff_id=username, name=username)
                 db.session.add(new_staff)
                 
                 # Redirect to a user dashboard
                 return redirect(url_for('staff_dashboard'))
 
-
         # Handle GET requests or form submission failure
         return render_template('signup.html')
     
-    def load_user(user_id):
-    # Load the user from the database using the user ID
-        return User.query.get(int(user_id))
-
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
@@ -99,75 +105,54 @@ def create_app(config_overrides={}):
             password = request.form.get('password')
             user_type = request.form['user_type']
 
-            # Perform login logic (e.g., validate credentials)
-            user = User.query.filter_by(username=username).first()
+            user = get_user_by_username(username)
 
             if user and user.check_password(password):
                 
                 if user_type == 'student':
-                    # Log in the user
-                    login_user(user)
-                    # Redirect to a student dashboard
-                    return redirect(url_for('student_dashboard'))
+                    # Log in the user and redirect to student dashboard if the student exists
+                    #if verify_student(username):
+                        login_user(user)
+                        return redirect(url_for('student_dashboard'))
 
                 elif user_type == 'staff':
-                    # Log in the user
+                    # Log in the user and redirect to staff dashboard if the student exists
+                    #if verify_staff(username):
                     login_user(user)
-                    # Redirect to a staff dashboard
                     return redirect(url_for('staff_dashboard'))
 
         # Handle GET requests or form submission failure
         return render_template('login.html')
 
+
     @app.route('/staff-dashboard', methods=['GET', 'POST'])
     def staff_dashboard():
 
         if request.method == 'POST':
-            course_code = request.form['course_code']
-            course_name = request.form['course_name']
-            credits = int(request.form['credits'])
-            rating = float(request.form['rating'])
-            semester = int(request.form['semester'])
-            year = int(request.form['year'])
 
-            new_course = Course(
-                code=course_code,
-                name=course_name,
-                credits=credits,
-                rating=rating,
-                semester=semester,
-                year=year
-            )
+            if get_course_by_courseCode(request.form['course_code']):
+                print(f"Course already exists")
 
-            db.session.add(new_course)
-            db.session.commit()
+            create_course(request.form['course_code'], request.form['course_name'], float(request.form['rating']), int(request.form['credits']), "-", int(request.form['semester']), int(request.form['year']))
 
         courses = Course.query.all()
         programs = Program.query.all()
         return render_template('staffDashboard.html', courses=courses, programs=programs)
     
+
     @app.route('/add_program', methods=['GET', 'POST'])
     def add_program():
 
         if request.method == 'POST':
-            program_name = request.form['program_name']
-            core_credits = int(request.form['core_credits'])
-            elective_credits = int(request.form['elective_credits'])
-            foun_credits = int(request.form['foun_credits'])
+            if get_program_by_name(request.form['program_name']):
+                print(f"Program already exists")
 
-            new_program = Program(
-                name=program_name,
-                core=core_credits,
-                elective=elective_credits,
-                foun=foun_credits
-            )
-
-            db.session.add(new_program)
-            db.session.commit()
+            create_program(request.form['program_name'], int(request.form['core_credits']), int(request.form['elective_credits']), int(request.form['foun_credits']))
 
         courses = Course.query.all()
         programs = Program.query.all()
         return render_template('staffDashboard.html', courses=courses, programs=programs)
+
 
     @app.route('/student-dashboard')
     def student_dashboard():
@@ -175,6 +160,7 @@ def create_app(config_overrides={}):
         programs = Program.query.all()
         return render_template('studentDashboard.html', courses=courses, programs=programs)
     
+
     @app.route('/search_programs', methods=['GET'])
     def search_programs():
         query = request.args.get('program_query', '')
@@ -184,8 +170,10 @@ def create_app(config_overrides={}):
 
         # Fetch the list of courses
         courses = Course.query.all()
+        #courses = get_all_courses(request.args.get('program_query', ''))
 
         return render_template('studentDashboard.html', courses=courses, programs=programs)
+
 
     @app.route('/search_courses', methods=['GET'])
     def search_courses():
@@ -201,9 +189,12 @@ def create_app(config_overrides={}):
 
         return render_template('studentDashboard.html', courses=courses, programs=programs)
 
+
     @app.route('/course_plan')
     def course_plan():
-        return render_template('coursePlan.html')
+        # Fetch the list of courses
+        courses = Course.query.all()
+        return render_template('coursePlan.html', courses=courses)
     
     
     @app.route('/enroll_program/<program_id>', methods=['POST'])
@@ -222,6 +213,7 @@ def create_app(config_overrides={}):
         """
         return redirect(url_for('student_dashboard'))
     
+
     @app.route('/add_to_course_history/<course_code>', methods=['POST'])
     def add_to_course_history(course_code):
         
@@ -239,6 +231,25 @@ def create_app(config_overrides={}):
         """
 
         return redirect(url_for('student_dashboard'))
+    
+
+    @app.route('/generate_course_plan', methods=['POST'])
+    def generate_course_plan():
+        plan_type = request.form.get('plan_type')
+
+        """
+        # Handle the selected plan type
+        if plan_type == 'fastest_graduation':
+            
+        elif plan_type == 'easy_courses':
+            
+        elif plan_type == 'prioritize_electives':
+           
+
+        """
+
+        # Render the appropriate template for the generated course plan
+        return render_template('generated_course_plan.html')
     
 
     app.app_context().push()
